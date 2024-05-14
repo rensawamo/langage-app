3e07e4c43217f22b5dc5a3ea0e865421ba8a3434
rensawamo <ren0112ngzk@gmail.com>
2024-05-14 10:27:33 +0900

    result view

--- packages/core/views/lib/widgets/quiz/result_page_widget.dart
+++ packages/core/views/lib/widgets/quiz/result_page_widget.dart
@@ -5,6 +5,7 @@ import 'package:core_views/extension/view+extention.dart';
 import 'package:core_views/utility/app_color_set.dart';
 import 'package:core_views/utility/text_styles.dart';
 import 'package:core_views/widgets/app_text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
@@ -53,45 +54,94 @@ class _ResultPageState extends State<ResultPageWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Quiz Results'),
-        ),
-        body: Center(
-          child: Container(
-            width: context.mediaQueryWidth * 0.95,
-            alignment: Alignment.center,
-            child: Table(
-              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-              children: [
-                TableRow(
-                  children: [
-                    TableCell(
-                      child: _buildCell('Quiz'),
-                    ),
-                    TableCell(child: _buildCell('Score')),
-                    TableCell(child: _buildCell('Voice')),
-                    TableCell(child: _buildCell('Favorite')),
-                  ],
-                ),
-                ...List.generate(
-                    widget.quizes.length,
-                    (index) => TableRow(
-                          children: [
-                            TableCell(
-                                child: _buildSubtitleCell(
-                                    widget.quizes[index].text, index)),
-                            TableCell(
-                                child: _buildSubtitleCell(
-                                    widget.scores[index]?.toString() ?? '未回答',
-                                    index)),
-                            TableCell(child: _buildVoiceCell(index)),
-                            TableCell(child: _buildFavoriteCell(index)),
-                          ],
-                        )),
-              ],
+        body: Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: AppText(
+            text: "結果 ${widget.totalScore} / ${widget.count}",
+            style: TextStyles.l(
+              color: _defaultColor.color(widget.mode),
+              type: widget.textType,
             ),
           ),
-        ));
+        ),
+        SizedBox(
+          height: context.mediaQueryHeight * .025,
+        ),
+        _table(),
+        SizedBox(
+          height: context.mediaQueryHeight * .025,
+        ),
+        Container(
+          child: InkWell(
+            onTap: () {
+              GoRouter.of(context).pop();
+            },
+            child: Container(
+              padding: EdgeInsets.only(
+                left: 48.0,
+                right: 48.0,
+                top: 10,
+                bottom: 10,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: _defaultColor.color(widget.mode),
+              ),
+              child: Text(
+                "トピックに戻る",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: _reverseColor.color(widget.mode),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+
+  Widget _table() {
+    return Center(
+        child: Container(
+      width: context.mediaQueryWidth * .95,
+      height: context.mediaQueryHeight * .65,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical, // 垂直方向のスクロールを有効にする
+        child: Table(
+          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+          children: [
+            TableRow(
+              children: [
+                TableCell(
+                  child: _buildCell('Quiz'),
+                ),
+                TableCell(child: _buildCell('Score')),
+                TableCell(child: _buildCell('Voice')),
+                TableCell(child: _buildCell('Star')),
+              ],
+            ),
+            ...List.generate(
+                widget.quizes.length,
+                (index) => TableRow(
+                      children: [
+                        TableCell(
+                            child: _buildSubtitleCell(
+                                widget.quizes[index].text, index)),
+                        TableCell(
+                            child: _buildSubtitleCell(
+                                widget.scores[index]?.toString() ?? '未回答',
+                                index)),
+                        TableCell(child: _buildVoiceCell(index)),
+                        TableCell(child: _buildFavoriteCell(index)),
+                      ],
+                    )),
+          ],
+        ),
+      ),
+    ));
   }
 
   Widget _buildVoiceCell(int index) => Container(
@@ -115,13 +165,20 @@ class _ResultPageState extends State<ResultPageWidget> {
             isFavorites[index] ? Icons.star : Icons.star_border,
             color: Colors.yellow[700],
           ),
-          onPressed: () => _updateFavoriteStatus(index),
+          onPressed: () async => {
+            setState(() => isFavorites[index] = !isFavorites[index]),
+            isFavorites[index]
+                ? await QuizFavoriteSql.delete(widget.quizes[index].text,
+                    widget.topicType.name, widget.installtype.name)
+                : await QuizFavoriteSql.insert(widget.quizes[index].text,
+                    widget.topicType.name, widget.installtype.name),
+          },
         ),
       );
 
   Widget _buildCell(String text) => Container(
-        height: 50,
-        padding: const EdgeInsets.all(8),
+        padding:
+            const EdgeInsets.only(top: 15, bottom: 10, left: 22, right: 22),
         color: Colors.green,
         alignment: Alignment.center,
         child: Text(text, style: TextStyle(color: Colors.white)),
@@ -134,11 +191,4 @@ class _ResultPageState extends State<ResultPageWidget> {
         alignment: Alignment.center,
         child: Text(text, style: TextStyle(color: Colors.black)),
       );
-
-  Future<void> _updateFavoriteStatus(int index) async {
-    setState(() {
-      isFavorites[index] = !isFavorites[index];
-    });
-    // SQLデータベース更新ロジックをここに追加
-  }
 }
