4e50ada16f015535a5624ed557270210ea7d69f7
rensawamo <ren0112ngzk@gmail.com>
2024-07-22 14:20:02 +0900

    chore: make install 追加

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-echo "Welcome to the Dart Frog project!"
+echo "Welcome to the languageapp project! 🐸"
 echo "Install dependencies!!!"
 brew update
-brew install mise 
+brew install mise
 brew install mason
-dart pub global activate melos
-dart pub add dart_frog
+brew install fvm
+fvm install 3.22.2
+fvm use 3.22.2
+fvm dart pub global activate melos
+melos bootstrap
